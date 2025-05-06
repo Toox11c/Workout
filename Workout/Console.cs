87e8044726f72af58cb3c2342bb8612fd112abef
Maxim <Maxim@DESKTOP-PGPVJD9>
2025-05-07 02:35:17 +0300

    Добавьте файлы проекта.

---
+++ Workout/Console.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Workout;
+
+namespace Console
+{
+    class Simulation : runner
+    {
+        public Runner Runner { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        static void Main()
+        {
+        var runner = GetRunnerFromUser();
+        int distance = GetDistanceFromUser();
+
+        }
+
+
+    }
+}
